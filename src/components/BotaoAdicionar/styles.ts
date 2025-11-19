83f1b5c79836d18be296a0f1560769293779988b
Cassiano <Cassianocosta12357@gmail.com>
2025-11-19 18:39:49 -0300

    update

--- src/components/BotaoAdicionar/styles.ts
+++ src/components/BotaoAdicionar/styles.ts
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
 
 export const Circulo = styled(Link)`
-  display: block;
+  display: flex;
   height: 64px;
   width: 64px;
-  background-color: #44bd32;
+  background-color: ${variaveis.verde};
   color: #fff;
   position: fixed;
+  border-radius: 50%;
   bottom: 40px;
   right: 40px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   font-size: 40px;
   text-decoration: none;
 `
